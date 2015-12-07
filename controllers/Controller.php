9582b22f58f9ca6e73acf987f2b6cb306ae6543e
alex83690 <alex83690@live.fr>
2015-12-07 15:35:46 +0100

    Commit par Alex

--- controllers/Controller.php
+++ controllers/Controller.php
@@ -1,5 +1,5 @@
 <?php
-include_once("models/Model.php");
+include_once('../models/Model.php');
 
 class Controller {
     public $model;
@@ -11,6 +11,11 @@ class Controller {
 
     public function invoke()
     {
+        include '../views/persists/head.php';
+        include '../views/persists/header.php';
+        include '../views/persists/nav.php';
         include '../views/index.php';
+        include '../views/persists/footer.php';
+        include '../views/persists/end.php';
     }
 }
