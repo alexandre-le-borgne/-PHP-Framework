4e0f8b1fe4f2cc815c2fb37d3538b0b07709ec14
darkicex3 <maxime.junique@gmail.com>
2015-12-07 16:57:02 +0100

    fb_connect

--- index.php
+++ index.php
@@ -1,12 +1,24 @@
 <?php
+
+//Gestion des erreurs
 error_reporting(E_ALL);
 ini_set('error_reporting', E_ALL);
-echo "error";
+
+//Facebook Connect
+
+use Facebook\FacebookRedirectLoginHelper;
+use Facebook\FacebookSessionPersistentDataHandler;
 
 require 'vendor/autoload.php';
 
 $appId = '1148334785195709';
 $appSecret = 'd8e2cb9e079641dcae34aca3e356bc4e';
+
+FacebookSession::setDefaultApplication($appId, $appSecret);
+$helper = new \Facebook\Helpers\FacebookRedirectLoginHelper('http://cuscom.fr/aaron/index.php');
+
+echo $helper->getLoginUrl();
+
 
 include_once("controllers/Controller.php");
 
