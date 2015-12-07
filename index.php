9a75abed6a7b317a1d398b48b2bc548e76e92a82
darkicex3 <maxime.junique@gmail.com>
2015-12-07 16:46:12 +0100

    fb_connect

--- index.php
+++ index.php
@@ -4,6 +4,10 @@ ini_set('error_reporting', E_ALL);
 echo "error";
 
 require 'vendor/autoload.php';
+
+$appId = '1148334785195709';
+$appSecret = 'd8e2cb9e079641dcae34aca3e356bc4e';
+
 include_once("controllers/Controller.php");
 
 $controller = new Controller();
