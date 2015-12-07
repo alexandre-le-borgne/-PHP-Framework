4f9b4f1715c16ff82afc0c760bda224e46bd169a
alex83690 <alex83690@live.fr>
2015-12-07 16:19:38 +0100

    Commit by Alex

--- controllers/Controller.php
+++ controllers/Controller.php
@@ -1,12 +1,12 @@
 <?php
-include_once("../models/Model.php");
+//include_once("../models/Model.php");
 
 class Controller {
     public $model;
 
     public function __construct()
     {
-        $this->model = new Model();
+   //     $this->model = new Model();
     }
 
     public function invoke()
