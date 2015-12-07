acd9ec0b76d74aba6f2f593704babd46df7c6638
darkicex3 <maxime.junique@gmail.com>
2015-12-07 16:44:01 +0100

    fb_connect

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ error_reporting(E_ALL);
 ini_set('error_reporting', E_ALL);
 echo "error";
 
+require 'vendor/autoload.php';
 include_once("controllers/Controller.php");
 
 $controller = new Controller();
