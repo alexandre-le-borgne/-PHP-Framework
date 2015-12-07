8ddda7408a681a13ce9b77e5292f794a3c1e50ca
alex83690 <alex83690@live.fr>
2015-12-07 16:59:20 +0100

    Commit by Alex

--- configs/config.php
+++ configs/config.php
@@ -1,1 +1,31 @@
 <?php
+
+class Configuration {
+
+    private static $parameters;
+
+    // Renvoie la valeur d'un paramètre de configuration
+    public static function get($name, $defaultValue = null) {
+        if (isset(self::getParameters()[$name])) {
+            $value = self::getParameters()[$name];
+        }
+        else {
+            $value = $defaultValue;
+        }
+        return $value;
+    }
+
+    // Renvoie le tableau des paramètres en le chargeant au besoin
+    private static function getParameters() {
+        if (self::$parameters == null) {
+            $filePath = "config.ini";
+            if (!file_exists($filePath)) {
+                throw new Exception("Aucun fichier de configuration trouvé");
+            }
+            else {
+                self::$parameters = parse_ini_file($filePath);
+            }
+        }
+        return self::$parameters;
+    }
+}
--- index.php
+++ index.php
@@ -1,9 +1,3 @@
 <?php
 error_reporting(E_ALL);
 ini_set('error_reporting', E_ALL);
-echo "error";
-
-include_once("controllers/Controller.php");
-
-$controller = new Controller();
-$controller->invoke();
