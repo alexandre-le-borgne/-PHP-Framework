448e5a695302ade67338de751ef43aafd58c84cb
Oominimaou2oO <alex83690@live.fr>
2015-12-14 15:35:54 +0100

    ok

---
+++ class/Controller.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: l14011190
+ * Date: 14/12/15
+ * Time: 14:17
+
+
+ */
+
+class Controller {
+
+}
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+//http://www.grafikart.fr/formations/programmation-objet-php/mvc-model-view-controller
 error_reporting(E_ALL);
 ini_set('error_reporting', E_ALL);
 
@@ -9,6 +9,8 @@ use Facebook\FacebookSessionPersistentDataHandler;
 
 require 'vendor/autoload.php';
 
+
+
 session_start();
 
 $appId = '1148334785195709';
@@ -16,6 +18,11 @@ $appSecret = 'd8e2cb9e079641dcae34aca3e356bc4e';
 
 FacebookSession::setDefaultApplication($appId, $appSecret);
 $helper = new \Facebook\Helpers\FacebookRedirectLoginHelper('http://cuscom.fr/aaron/index.php');
+
+$router = new Router($_GET['url']);
+$router->addGet('/', function(){ echo "Bienvenue sur ma homepage !"; });
+$router->addGet('/posts/:id', function($id){ echo "Voila l'article $id"; });
+$router->run();
 
 //echo $helper->getLoginUrl();
 
