8c174f2b84e8e9ee386a2a4677f63a7afafd0cbe
j14003626 <theo.jeanjean42@gmail.com>
2015-12-16 14:18:03 +0100

    Register Form

--- views/forms/registerForm.php
+++ views/forms/registerForm.php
@@ -4,11 +4,52 @@ un formulaire d’inscription pour les internautes souhaitant créer un compte p
 
 minimum :
 
-o d’enregistrer en base de données, obligatoirement, le nom complet (ou pseudonyme),
+- d’enregistrer en base de données, obligatoirement, le nom complet (ou pseudonyme),
 
-o d’envoyer un e-mail de confirmation d’inscription à l’internaute ;
+- d’envoyer un e-mail de confirmation d’inscription à l’internaute ;
 
 e-mail et mot de passe (encodé) et éventuellement d’autres informations de
 
 l’internaute ;
 */
+?>
+
+<form method="post" name="register">
+    <div class="formRegister">
+        <input type="text" name="username" placeholder="Pseudonyme" requiered>
+    </div>
+    <div class="formRegister">
+        <input type="email" name="email" placeholder="E-mail" requiered>
+    </div>
+    <div class="formRegister">
+        <input type="text" name="password" placeholder="Mot de passe" requiered>
+    </div>
+    <div class="formRegister">
+        <input type="text" name="pwdConfirm" placeholder="Mot de passe" requiered>
+    </div>
+    <div class="formRegister">
+        <input type="date" name="birthDate" placeholder="Date de naissance" requiered>
+    </div>
+
+    <!-- pour que les bots se trompent -->
+    <input type="submit" class="hide">
+    <input type="hidden" name="register">
+    <fieldset id="captchafield">
+        <div id="captcha"></div>
+    </fieldset>
+
+</form>
+
+
+<script type="text/javascript">
+    $(function() {
+        var s = new Slider("captchafield",{
+            message: "Glissez pour créer le compte",
+            handler: function(){
+                $("#captchafield").hide("slow");
+                document.register.submit();
+            }
+        });
+        s.init();
+    });
+</script>
