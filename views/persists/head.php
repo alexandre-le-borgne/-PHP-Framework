a3b441315485311d63c969242b9535272e3fc713
Alexandre <alex83690@live.fr>
2015-12-16 17:29:33 +0100

    Alex

--- views/persists/head.php
+++ views/persists/head.php
@@ -9,10 +9,10 @@
         <link rel="stylesheet" type="text/css" href="jquery.fullPage.css" />
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
         <!-- This following line is optional. Only necessary if you use the option css3:false and you want to use other easing effects rather than "linear", "swing" or "easeInOutCubic". -->
-        <script src="../../web/js/fullPage/vendors/jquery.easings.min.js"></script>
+        <script src="web/js/fullPage/vendors/jquery.easings.min.js"></script>
         <!-- This following line is only necessary in the case of using the plugin option `scrollOverflow:true` -->
-        <script type="text/javascript" src="../../web/js/fullPage/vendors/jquery.slimscroll.min.js"></script>
-        <script type="text/javascript" src="../../web/js/fullPage/jquery.fullPage.js"></script>
-        <script src="../../web/js/script.js"></script>
+        <script type="text/javascript" src="web/js/fullPage/vendors/jquery.slimscroll.min.js"></script>
+        <script type="text/javascript" src="web/js/fullPage/jquery.fullPage.js"></script>
+        <script src="web/js/script.js"></script>
     </head>
     <body>
