31c92b2aba7081f7c5ebec25efe5febe99ab0797
darkicex3 <maxime.junique@gmail.com>
2015-12-16 14:10:50 +0100

    fb_connect

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ error_reporting(E_ALL);
 ini_set('error_reporting', E_ALL);
 
 require 'class/Router.php';
+
 $router = new Router($_GET['url']);
 $router->addGet('/', function(){ echo "Bienvenue sur ma homepage !"; });
 $router->addGet('/posts/:id', function($id){ echo "Voila l'article $id"; });
