bc3fe3eae6165f59cfa0fe8e1adda90ad0b1705a
darkicex3 <maxime.junique@gmail.com>
2015-12-16 14:07:16 +0100

    fb_connect

--- index.php
+++ index.php
@@ -4,21 +4,6 @@ error_reporting(E_ALL);
 ini_set('error_reporting', E_ALL);
 
 
-use Facebook\FacebookRedirectLoginHelper;
-use Facebook\FacebookSessionPersistentDataHandler;
-
-require 'vendor/autoload.php';
-
-
-
-session_start();
-
-$appId = '1148334785195709';
-$appSecret = 'd8e2cb9e079641dcae34aca3e356bc4e';
-
-FacebookSession::setDefaultApplication($appId, $appSecret);
-$helper = new \Facebook\Helpers\FacebookRedirectLoginHelper('http://cuscom.fr/aaron/index.php');
-
 $router = new Router($_GET['url']);
 $router->addGet('/', function(){ echo "Bienvenue sur ma homepage !"; });
 $router->addGet('/posts/:id', function($id){ echo "Voila l'article $id"; });
