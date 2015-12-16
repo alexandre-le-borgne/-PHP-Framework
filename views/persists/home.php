098237fa3c1c6204ef8192fdd93c6fdf4f2213af
darkicex3 <maxime.junique@gmail.com>
2015-12-16 14:26:25 +0100

    fb_connect

--- views/persists/home.php
+++ views/persists/home.php
@@ -10,7 +10,7 @@
     <h1>Mon super blog !</h1>
     <p>Derniers billets du blog :</p>
 
-    <?php include '../views/registerForm.php' ?>
+    <?php include '../forms/registerForm.php' ?>
 
 </body>
 </html>
