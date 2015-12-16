f6fb4933c5f3383e0156b273422d675cbb8284f7
j14003626 <theo.jeanjean42@gmail.com>
2015-12-16 14:38:08 +0100

    Register Form

--- views/persists/head.php
+++ views/persists/head.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <title>ProjetPHP - Aaron</title>
         <link rel="stylesheet" href="../../web/css/body.css">
+        <link rel="stylesheet" href="../../web/css/form.css">
         <script src="../../web/js/script.js"></script>
     </head>
     <body>
