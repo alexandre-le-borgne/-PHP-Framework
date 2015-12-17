8d236268fc9c4f45b59d867ac1c7f97c1428bcf4
j13000355 <julienjandot@orange.fr>
2015-12-17 14:53:55 +0100

    IndexModel.php

--- model/IndexModel.php
+++ model/IndexModel.php
@@ -8,8 +8,30 @@
  */
 class IndexModel extends Model
 {
-    public function availableAccount() {
+
+    public function availableAccount($username) {
+
         $db = new Database();
+
+        $user = Securite::escape($username);
+
+        $sql = "Select * From User Where username = '$user'";
+
+        return ($db->execute($sql) == NULL);
+    }
+
+    public function addUser($username, $email, $password, $birthdate){
+        $db = new Database();
+
+        $username = Securite::escape($username);
+        $email = Securite::escape($email);
+        $password = Securite::escape($password);
+        $birthdate = Securite::escape($birthdate);
+
+        $password = Securite::encode($password);
+
+        $insert = "Insert Into User ('username', 'email', 'password', 'birthDate') Values ('$username', '$email', '$password', '$birthdate')";
+        $db->execute($insert);
 
     }
 }
