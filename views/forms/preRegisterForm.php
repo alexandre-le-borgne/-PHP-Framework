d115c98964fa25ebba7082631f14a276e4edd96c
darkicex3 <maxime.junique@gmail.com>
2015-12-17 16:28:08 +0100

    Merge branch 'master' of /Applications/XAMPP/htdocs/ProjectPHP with conflicts.

--- views/forms/preRegisterForm.php
+++ views/forms/preRegisterForm.php
@@ -4,6 +4,7 @@
  * Comporte seulement username,email et mot de passe
  * puis redirige vers registerForm, plus complet avec les champs username,email et mot de passe
  * déjà remplis
+ *
  */
 ?>
 
@@ -12,35 +13,53 @@
     <h2><strong>Nouveau sur Aaron ?</strong> Inscrivez-vous ! </h2>
 
     <!--PRE SIGN UP FORM-->
-    <form class="form-horizontal" method="post" name="register" action="preregister">
+    <form class="form-horizontal" method="post" name="register" action="index.php?controller=index&action=register">
         <!--USERNAME-->
         <div class="control-group">
             <div class="controls">
                 <div class="input-prepend">
                     <span class="add-on">@</span>
-                    <input class="span2" id="prependedInput" name="username" type="text" placeholder="Username"
-                           required>
+                    <input class="span2" id="prependedInput" name="username" type="text" placeholder="Username" required>
                 </div>
             </div>
         </div>
+
+        <?php
+        if(isset($errors[username]))
+        {
+            ?>
+            <div class="control-group info">
+                <label class="control-label" for="username"><?php $errors[username]?></label>
+                <div class="controls">
+                    <input type="text" id="inputInfo">
+                    <span class="help-inline">Username is already taken</span>
+                </div>
+            </div>
+            <?php
+        }
+        ?>
+
         <!--EMAIL-->
         <div class="control-group">
             <div class="controls">
-                <input type="text" id="inputEmail" name="email" placeholder="Email" required>
+                <input type="text" id="inputEmail" placeholder="Email" required>
             </div>
         </div>
+
         <!--PASSWORD-->
         <div class="control-group">
             <div class="controls">
-                <input type="password" id="inputPassword" name="password" placeholder="Mot de passe" required>
+                <input type="password" id="inputPassword" placeholder="Mot de passe" required>
             </div>
         </div>
-        <!--CONFIRM PASSWORD-->
+
+        <!--PASSWORD-->
         <div class="control-group">
             <div class="controls">
-                <input type="password" id="inputPassword" name="confirmPwd" placeholder="Confirmez votre mot de passe">
+                <input type="password" id="inputPassword" placeholder="Mot de passe" required>
             </div>
         </div>
+
         <!--SUBMIT ACTION-->
         <div class="control-group">
             <div class="controls">
