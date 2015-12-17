2ca55e12dc7dcfdef3050cd569e48841847087f9
j14003626 <theo.jeanjean42@gmail.com>
2015-12-17 13:44:43 +0100

    ForgotForm.phjp

--- views/forms/forgotForm.php
+++ views/forms/forgotForm.php
@@ -1,8 +1,25 @@
 <?php
 /**
-un système de mot de passe oublié ou perdu (au choix : possibilité de réinitialiser un
-
-nouveau mot de passe avec une question secrète et/ou par e-mail, génération automatique
-
-d’un mot de passe temporaire ou non, etc.)
+ * un système de mot de passe oublié ou perdu pour réinitialiser mot de passe
+ * par mail.
+ * generation automatique d'un nouveau mot de passe, l'utilisateur pourra le changer
+ * sur sa gestion de compte.
  */
+?>
+
+<div class="forgotDiv">
+    <h2><strong>Retrouvez votre mot de passe !</strong></h2>
+
+    <form class="form-horizontal" method="post" name="register">
+        <div class="control-group">
+            <div class="controls">
+                <input type="email" id="inputEmail" placeholder="Email">
+            </div>
+        </div>
+        <div class="control-group">
+            <div class="controls">
+                <button type="submit" name="action" value="forgotPwd" class="btn">Sign in</button>
+            </div>
+        </div>
+    </form>
+</div>
