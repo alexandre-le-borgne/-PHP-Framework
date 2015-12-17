504dcaaac6c5ee3988c0e51d8aa975240c1fbe22
darkicex3 <maxime.junique@gmail.com>
2015-12-17 16:46:58 +0100

    login bootstrap

--- views/forms/loginForm.php
+++ views/forms/loginForm.php
@@ -6,24 +6,6 @@
  */
 ?>
 
-<!-- w=302, h=119 -->
-<!---->
-<!--            <input type="text" name="username" placeholder="Pseudonyme" required>-->
-<!--            <input type="password" name="password" placeholder="Mot de passe" required>-->
-<!--            <button type="submit" class="buttonSignup">Se connecter</button>-->
-<!---->
-<!---->
-<!--        <div class="rememberAndForgot">-->
-<!--            <label class="remember">-->
-<!--                <input type="checkbox" value="1" name="rememberMe">-->
-<!--                <span>Se souvenir de moi</span>-->
-<!--            </label>-->
-<!--            <span class="separator">·</span>-->
-<!--            <a class="forgot" href="./forgotForm.php">Mot de passe oublié ?</a>-->
-<!--        </div>-->
-<!---->
-<!--    </form>-->
-
 <div class="loginDiv">
 
     <h2>Connectez-vous ?</h2>
@@ -31,23 +13,13 @@
     <!--SIGN IN FORM-->
     <form class="form-horizontal" method="post" name="register">
         <!--USERNAME-->
-
-            <div class="controls">
-                <input type="text" placeholder="Identifiant">
-            </div>
-            <div class="controls">
-                <input type="password" id="inputPassword" placeholder="Password">
-            </div>
-            <div class="controls">
-                <button type="submit" class="btn">Sign in</button>
-            </div>
-            <div class="controls">
-                <label class="checkbox">
-                    <input type="checkbox"> Remember me
-                </label>
-                <a class="forgot" href="./forgotForm.php">Mot de passe oublié ?</a>
-            </div>
-
+        <input type="text" placeholder="Identifiant">
+        <input type="password" id="inputPassword" placeholder="Password">
+        <button type="submit" class="btn">Sign in</button>
+        <label class="checkbox">
+            <input type="checkbox"> Remember me
+        </label>
+        <a class="forgot" href="./forgotForm.php">Mot de passe oublié ?</a>
         <hr>
         <!--SOCIAL CONNECT-->
         <div class="socialConnect">
