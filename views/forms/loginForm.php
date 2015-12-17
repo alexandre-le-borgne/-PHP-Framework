af2493cb373fbccb69f3e03e48a0d34eb0cce1ac
darkicex3 <maxime.junique@gmail.com>
2015-12-17 13:21:25 +0100

    login bootstrap

--- views/forms/loginForm.php
+++ views/forms/loginForm.php
@@ -6,29 +6,48 @@
  */
 ?>
 
-<div class="loginDiv"><!-- w=302, h=119 -->
+<!-- w=302, h=119 -->
 
-    <form class="loginForm" method="post" name="register"><!-- w=276, h=99 -->
 
-        <div class="field"><!-- w=276, h=30  + margin-bottom=? -->
-            <input type="text" name="username" placeholder="Pseudonyme" required>
+<!---->
+<!--            <input type="text" name="username" placeholder="Pseudonyme" required>-->
+<!--            <input type="password" name="password" placeholder="Mot de passe" required>-->
+<!--            <button type="submit" class="buttonSignup">Se connecter</button>-->
+<!---->
+<!---->
+<!--        <div class="rememberAndForgot">-->
+<!--            <label class="remember">-->
+<!--                <input type="checkbox" value="1" name="rememberMe">-->
+<!--                <span>Se souvenir de moi</span>-->
+<!--            </label>-->
+<!--            <span class="separator">·</span>-->
+<!--            <a class="forgot" href="./forgotForm.php">Mot de passe oublié ?</a>-->
+<!--        </div>-->
+<!---->
+<!--    </form>-->
+
+<div class="loginDiv">
+    <form class="form-horizontal" method="post" name="register">
+        <div class="control-group">
+            <div class="controls">
+                <input type="text" id="inputEmail" placeholder="Email">
+            </div>
         </div>
-
-        <div class="field">
-            <input type="password" name="password" placeholder="Mot de passe" required>
-            <button type="submit" class="buttonSignup">Se connecter</button>
+        <div class="control-group">
+            <div class="controls">
+                <input type="password" id="inputPassword" placeholder="Password">
+            </div>
         </div>
-
-        <div class="rememberAndForgot">
-            <label class="remember">
-                <input type="checkbox" value="1" name="rememberMe">
-                <span>Se souvenir de moi</span>
-            </label>
-            <span class="separator">·</span>
-            <a class="forgot" href="./forgotForm.php">Mot de passe oublié ?</a>
+        <div class="control-group">
+            <div class="controls">
+                <label class="checkbox">
+                    <input type="checkbox"> Remember me
+                </label>
+                <button type="submit" class="btn">Sign in</button>
+            </div>
         </div>
-
     </form>
+</div>
 
     <div class="socialConnect">
 
