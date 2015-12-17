e74c2c37ca0d198df7022ef6bbcf279941fc67f6
j14003626 <theo.jeanjean42@gmail.com>
2015-12-17 14:00:51 +0100

    ForgotForm.phjp

--- views/forms/forgotForm.php
+++ views/forms/forgotForm.php
@@ -18,7 +18,7 @@
         </div>
         <div class="control-group">
             <div class="controls">
-                <button type="submit" name="action" value="forgotPwd" class="btn">Réinitialiser</button>
+                <button type="submit" name="action" value="forgotPwd" class="btn">R&eacute;initialiser</button>
             </div>
         </div>
     </form>
