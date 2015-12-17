e6615b3cb85764a17c567c8ffc190c280cebb2a4
darkicex3 <maxime.junique@gmail.com>
2015-12-17 13:25:12 +0100

    login bootstrap

--- views/persists/head.php
+++ views/persists/head.php
@@ -3,7 +3,10 @@
     <head>
         <meta charset="utf-8">
         <title>ProjetPHP - Aaron</title>
-        <link rel="stylesheet" href="web/css/*">
+        <link rel="stylesheet" href="web/css/bootstrap.css">
+        <link rel="stylesheet" href="web/css/bootstrap.min.css">
+        <link rel="stylesheet" href="web/css/bootstrap-responsive.css">
+        <link rel="stylesheet" href="web/css/bootstrap-responsive.min.css">
         <link rel="stylesheet" type="text/css" href="jquery.fullPage.css" />
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
         <!-- This following line is optional. Only necessary if you use the option css3:false and you want to use other easing effects rather than "linear", "swing" or "easeInOutCubic". -->
