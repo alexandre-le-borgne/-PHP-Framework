de52f773c327b227ddcd62600ca9972d982a06f6
Alexandre <alex83690@live.fr>
2015-12-18 09:46:01 +0100

    Alex

--- views/layouts/exception.php
+++ views/layouts/exception.php
@@ -5,6 +5,24 @@
  * Date: 18/12/2015
  * Time: 09:17
  */
-echo '<h1>Une exception est survenue!</h1><br><hr><br><br><pre>';
-var_dump($trace);
-echo '</pre>';
+?>
+<style>
+    header {
+
+    }
+</style>
+<header>
+    <img src="https://raw.githubusercontent.com/poulfoged/WebApiExceptionPipeline/master/logo.png">
+    <div>
+        <h1>Une exception est survenue !</h1>
+        <h2><?= $name ?></h2>
+    </div>
+</header>
+<main>
+    <h3>[<?= $code ?>] <?= $message ?> [<?= $file ?> - Ligne <?= $line ?>]</h3>
+    <pre>
+        <?php
+        var_dump($trace);
+        ?>
+    </pre>
+</main>
