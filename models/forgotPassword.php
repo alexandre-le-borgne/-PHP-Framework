c249f54d305bb8c947ebfcd3a55aed214c17fced
jikar <julienjandot@orange.fr>
2015-12-29 11:26:23 +0100

    forgotPassword

--- models/forgotPassword.php
+++ models/forgotPassword.php
@@ -6,11 +6,3 @@
  * Time: 13:36
  */
 
-    $resultat = Database::execute($requete, array(':calories' => 150, ':couleur' => 'red'));
-
-    if($_POST['action'] == 'forgotPwd'){
-        $email = Securite::insertBD($_POST['email']);
-
-        $sujet = "Demande nouveau mot de passe";
-        $texte = "cc";
-    }
