3ac81bfac621be42bde8e6148ee8d8442ed5c6ba
j14003626 <dzdada@gmail.coml>
2016-01-04 14:10:42 +0100

    coucou

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 error_reporting(E_ALL);
 ini_set('error_reporting', E_ALL);
 
+/**
+ * Les fonctions principales pour le fonctionnement du MVC
+ */
 require_once 'app/TraceableException.php';
 require_once 'app/Session.php';
 require_once 'app/Request.php';
@@ -12,12 +15,18 @@ require_once 'app/Database.php';
 require_once 'app/Model.php';
 require_once 'app/Route.php';
 require_once 'app/Router.php';
+require_once 'app/Mail.php';
 require_once 'app/Security.php';
 
-/**/
+/**
+ * Les Differents controlleurs
+ */
 require_once 'controller/IndexController.php';
 require_once 'controller/UserController.php';
 
+/**
+ * Les Differents modeles
+ */
 require_once 'model/IndexModel.php';
 require_once 'model/PostModel.php';
 require_once 'model/FeedRSSModel.php';
