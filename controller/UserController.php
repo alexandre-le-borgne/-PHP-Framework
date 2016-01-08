701a231d83d42abf7fbc1ad47d303926deccc147
j13000355 <julienjandot@orange.fr>
2016-01-08 14:10:12 +0100

    h

--- controller/UserController.php
+++ controller/UserController.php
@@ -90,4 +90,23 @@ class UserController extends Controller
     {
         $this->render('persists/home');
     }
+
+    public function MailResetAction(){
+
+        $email = Security::escape($_POST['inputEmail']);
+
+        $req = "Select username, userKey From accounts Where email = $email";
+
+        $db = new Database();
+
+        $data = $db->execute($req)->fetch();
+
+        $user = $data['username'];
+        $key = $data['userKey'];
+
+        Mail::sendResetMail($email, $user, $key);
+
+        echo "Un mail vous a été envoyé à votre adresse d'inscription, merci de suivre les instructions qu'il renferme";
+    }
+
 }
--- views/forms/mailValidation.php
+++ views/forms/mailValidation.php
@@ -7,7 +7,7 @@ $user = $_GET['user'];
 $key = $_GET['key'];
 
 $db = new Database();
-$req = "Select email, userKey, active From User Where username like :user";
+$req = "Select email, userKey, active From accounts Where username like :user";
 
 if($db->execute($req, $user) && $data = $db->execute($req, $user)->fetch())
 {
@@ -23,7 +23,7 @@ else
     if($key == $realKey)
     {
         echo "Votre compte a bien été activé";
-        $req = "Update User Set active = 1 Where username like :user";
+        $req = "Update accounts Set active = 1 Where username like :user";
         $db->execute($req, $user);
         Mail::sendWelcomingMail($email);
     }
