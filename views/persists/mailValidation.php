232bb4a68ffc5b755c8c4213b6182d26e4a73ed7
j13000355 <julienjandot@orange.fr>
2016-01-08 15:34:02 +0100

    h

--- views/persists/mailValidation.php
+++ views/persists/mailValidation.php
@@ -3,7 +3,11 @@
  * Validation du mail (Model ?)
  */
 
+View::getView('persists/head');
+
 if (isset($message))
     echo $message;
 else
     View::getView('index');
+
+View::getView('persists/end');
--- views/persists/validateInscription.php
+++ views/persists/validateInscription.php
@@ -4,6 +4,8 @@
  * le compte a ete cree.
  */
 
-
+View::getView('persists/head');
 echo "Un email vous a été envoyé, rendez vous sur votre boite mail pour valider l'inscription";
+View::getView('persists/end');
+
 
