39164602dd53f5e3a5b6ed30dbd33379cd0eec0f
Alexandre <alex83690@live.fr>
2016-01-12 22:44:33 +0100

    Alex

--- app/View.php
+++ app/View.php
@@ -20,6 +20,7 @@ class View
     }
 
     public function render($view, $data = array()) {
+        echo "t".$view."<br>";
         $datacopy = $data;
         $data['view'] = $this;
         $viewspath = __DIR__.DIRECTORY_SEPARATOR.'../views/';
