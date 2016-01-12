ad0ca27d3673711a5734089db3bf2f1c2d37fb22
Alexandre <alex83690@live.fr>
2016-01-12 22:54:10 +0100

    Alex

--- controller/ExempleController.php
+++ controller/ExempleController.php
@@ -10,6 +10,6 @@ class ExempleController extends Controller
 {
     public function IndexAction()
     {
-        $this->render('exemple/home', array("title" => "Mon titre", "content" => "Mon contenu"));
+        $this->render('exemple/home', array("title" => "Mon titre de test", "content" => "Mon contenu"));
     }
 }
