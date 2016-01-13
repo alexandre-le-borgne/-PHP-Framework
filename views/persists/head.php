49f1507da232299db6cd6a4fbaf4b103f616a3a7
Alexandre <alex83690@live.fr>
2016-01-13 11:26:41 +0100

    Alex

--- views/persists/head.php
+++ views/persists/head.php
@@ -6,7 +6,7 @@
         <link rel="stylesheet" href="<?= View::getAsset('css/bootstrap.css') ?>">
         <link rel="stylesheet" href="<?= View::getAsset('css/bootstrap-responsive.css') ?>">
         <link rel="stylesheet" href="<?= View::getAsset('css/bootstrap-responsive.min.css') ?>">
-        <link rel="stylesheet" href="<?= View::getAsset('js/fullPage/jquery.fullPage.css') ?>" />
+        <link rel="stylesheet" href="<?= View::getAsset('inc/fullPage/jquery.fullPage.css') ?>" />
         <link rel="stylesheet" href="<?= View::getAsset('css/body.css') ?>">
         <link rel="stylesheet" href="<?= View::getAsset('css/footer.css') ?>">
         <link rel="stylesheet" href="<?= View::getAsset('css/form.css') ?>">
@@ -14,10 +14,10 @@
 
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
         <!-- This following line is optional. Only necessary if you use the option css3:false and you want to use other easing effects rather than "linear", "swing" or "easeInOutCubic". -->
-        <script src="<?= View::getAsset('js/fullPage/vendors/jquery.easings.min.js') ?>"></script>
+        <script src="<?= View::getAsset('inc/fullPage/vendors/jquery.easings.min.js') ?>"></script>
         <!-- This following line is only necessary in the case of using the plugin option `scrollOverflow:true` -->
-        <script type="text/javascript" src="<?= View::getAsset('js/fullPage/vendors/jquery.slimscroll.min.js') ?>"></script>
-        <script type="text/javascript" src="<?= View::getAsset('js/fullPage/jquery.fullPage.js') ?>"></script>
+        <script type="text/javascript" src="<?= View::getAsset('inc/fullPage/vendors/jquery.slimscroll.min.js') ?>"></script>
+        <script type="text/javascript" src="<?= View::getAsset('inc/fullPage/jquery.fullPage.js') ?>"></script>
         <script type="text/javascript" src="<?= View::getAsset('js/slide.js') ?>"></script>
         <script type="text/javascript" src="<?= View::getAsset('js/script.js') ?>"></script>
     </head>
