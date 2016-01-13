f3e65ef21fd906dcfd59a975066947b5f197875d
j14003626 <dzdadaza>
2016-01-13 15:30:32 +0100

    caczdazdzad

--- controller/TestTwitterController.php
+++ controller/TestTwitterController.php
@@ -9,6 +9,7 @@ class TestTwitterController extends Controller
 {
     function TwitterAction()
     {
+        echo "coucou";
         //Consumer key,
         $oauth = new TwitterOAuth("rC3gP2pji5zoKoGf4FlUYdvaa", "TYIpFvcb9wR6SrpdxmMCPruiyJSPSDfJdLz6cAlNgqoyMcMq2j");
         $accesstoken = $oauth->oauth2('oauth2/token', ['grant_type' => 'client_credentials']);
