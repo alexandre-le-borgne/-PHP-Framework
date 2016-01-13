e7bbd391a1315f4b05d9ed3e86be5324e0280746
j13000355 <f>
2016-01-13 15:41:54 +0100

    testFb

--- views/persists/layout.php
+++ views/persists/layout.php
@@ -12,11 +12,34 @@
         <?php $this->render('persists/head'); ?>
     </head>
     <body>
+        <script>
+            window.fbAsyncInit = function() {
+                FB.init({
+                    appId      : '1695359537375763',
+                    xfbml      : true,
+                    version    : 'v2.5'
+                });
+            };
+
+            (function(d, s, id){
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) {return;}
+                js = d.createElement(s); js.id = id;
+                js.src = "//connect.facebook.net/en_US/sdk.js";
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+        </script>
         <div id="fullpage">
             <div class="section">
                 <?php
                 echo $this->output('_content');
                 ?>
+            </div>
+            <div
+                class="fb-like"
+                data-share="true"
+                data-width="450"
+                data-show-faces="true">
             </div>
             <div class="section section_news">
                 <div id="title_section_news">
