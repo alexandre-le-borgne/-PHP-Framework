b0a2404144c0f7f8353da726b534446a8922f3b2
Alexandre <alex83690@live.fr>
2016-01-13 14:21:19 +0100

    Alex

--- views/exemple/layout.php
+++ views/exemple/layout.php
@@ -19,9 +19,9 @@
     $view->render('exemple/head', array('title' => $title));
     ?>
     <body>
-        <h1><?= $view->output('title', 'Titre par défaut') ?></h1>
-        <h2><?= $view->output('title2', 'Sous-Titre par défaut') ?></h2>
-        <?php if($view->isGranted(Session::USER_IS_CONNECTED)): ?>
+        <h1><?= $this->output('title', 'Titre par défaut') ?></h1>
+        <h2><?= $this->output('title2', 'Sous-Titre par défaut') ?></h2>
+        <?php if($this->isGranted(Session::USER_IS_CONNECTED)): ?>
             L'utilisateur est connecté !
         <?php endif; ?>
         <?= $_content ?>
