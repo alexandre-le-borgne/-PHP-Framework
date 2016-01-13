c79677234df6648299892ab1147115e395ebc274
j14003626 <dzdadaza>
2016-01-13 16:09:47 +0100

    f

--- controller/TestTwitterController.php
+++ controller/TestTwitterController.php
@@ -8,7 +8,7 @@
 
 use Abraham\TwitterOAuth\TwitterOAuth;
 
-require __DIR__ . '/../vendor/abraham/twitteroauth/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 //require '/../vendor/abraham/twitteroauth/autoload.php';
 
