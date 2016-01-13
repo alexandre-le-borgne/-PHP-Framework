6ffca0dd05caafa8ad623ae796a6c1477885d5df
j14003626 <dzdadaza>
2016-01-13 15:47:52 +0100

    caczdazdzad

--- controller/TestTwitterController.php
+++ controller/TestTwitterController.php
@@ -5,7 +5,7 @@
 
 use Abraham\TwitterOAuth\TwitterOAuth;
 
-//require '../vendor/autoload.php';
+require '../vendor/abraham/twitteroauth/autoload.php';
 
 class TestTwitterController extends Controller
 {
