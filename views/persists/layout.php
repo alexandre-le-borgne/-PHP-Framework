3198d3f6b971a5f815969566e9da508fc5c7029c
j13000355 <f>
2016-01-13 15:52:15 +0100

    testFb

--- views/persists/layout.php
+++ views/persists/layout.php
@@ -21,6 +21,7 @@
                 });
             };
 
+
             (function(d, s, id){
                 var js, fjs = d.getElementsByTagName(s)[0];
                 if (d.getElementById(id)) {return;}
