e2e607beae37bba0ad73f05ee4aaa7d62b8f213e
Alexandre <alex83690@live.fr>
2016-01-13 11:24:47 +0100

    Alex

--- views/persists/head.php
+++ views/persists/head.php
@@ -3,23 +3,22 @@
     <head>
         <meta charset="utf-8">
         <title>ProjetPHP - Aaron</title>
-        <link rel="stylesheet" href="/web/css/bootstrap.css">
-        <link rel="stylesheet" href="web/css/bootstrap.min.css">
-        <link rel="stylesheet" href="web/css/bootstrap-responsive.css">
-        <link rel="stylesheet" href="web/css/bootstrap-responsive.min.css">
-        <link rel="stylesheet" href="web/js/fullPage/jquery.fullPage.css" />
-        <link rel="stylesheet" href="web/css/body.css">
-        <link rel="stylesheet" href="web/css/footer.css">
-        <link rel="stylesheet" href="web/css/form.css">
-        <link rel="stylesheet" href="web/css/nav.css">
+        <link rel="stylesheet" href="<?= View::getAsset('css/bootstrap.css') ?>">
+        <link rel="stylesheet" href="<?= View::getAsset('css/bootstrap-responsive.css') ?>">
+        <link rel="stylesheet" href="<?= View::getAsset('css/bootstrap-responsive.min.css') ?>">
+        <link rel="stylesheet" href="<?= View::getAsset('js/fullPage/jquery.fullPage.css') ?>" />
+        <link rel="stylesheet" href="<?= View::getAsset('css/body.css') ?>">
+        <link rel="stylesheet" href="<?= View::getAsset('css/footer.css') ?>">
+        <link rel="stylesheet" href="<?= View::getAsset('css/form.css') ?>">
+        <link rel="stylesheet" href="<?= View::getAsset('css/nav.css') ?>">
 
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
         <!-- This following line is optional. Only necessary if you use the option css3:false and you want to use other easing effects rather than "linear", "swing" or "easeInOutCubic". -->
-        <script src="web/js/fullPage/vendors/jquery.easings.min.js"></script>
+        <script src="<?= View::getAsset('js/fullPage/vendors/jquery.easings.min.js') ?>"></script>
         <!-- This following line is only necessary in the case of using the plugin option `scrollOverflow:true` -->
-        <script type="text/javascript" src="web/js/fullPage/vendors/jquery.slimscroll.min.js"></script>
-        <script type="text/javascript" src="web/js/fullPage/jquery.fullPage.js"></script>
-        <script type="text/javascript" src="web/js/slide.js"></script>
-        <script src="web/js/script.js"></script>
+        <script type="text/javascript" src="<?= View::getAsset('js/fullPage/vendors/jquery.slimscroll.min.js') ?>"></script>
+        <script type="text/javascript" src="<?= View::getAsset('js/fullPage/jquery.fullPage.js') ?>"></script>
+        <script type="text/javascript" src="<?= View::getAsset('js/slide.js') ?>"></script>
+        <script type="text/javascript" src="<?= View::getAsset('js/script.js') ?>"></script>
     </head>
     <body>
