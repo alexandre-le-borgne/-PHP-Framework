5df0aef5f78785fe00a162f9c0fef53131985055
darkicex3 <maxime.junique@gmail.com>
2016-01-13 15:24:18 +0100

    home design

--- app/Controller.php
+++ app/Controller.php
@@ -5,6 +5,10 @@
  * User: l14011190
  * Date: 14/12/15
  * Time: 14:17
+ *
+ * Server > ftp.cluster011.ovh.net
+Username > cuscomfrmk-admin
+Mdp > MAXsky1995
  */
 
 abstract class Controller
---
+++ controller/testGmailcontroller.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maxbook
+ * Date: 13/01/16
+ * Time: 15:21
+ */
+
