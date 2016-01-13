c1381d1ca11c1e1f7abb93cc98f640513a1de427
j14003626 <dzdadaza>
2016-01-13 16:05:08 +0100

    f

--- controller/TestTwitterController.php
+++ controller/TestTwitterController.php
@@ -14,7 +14,7 @@ class TestTwitterController extends Controller
 {
     function TwitterAction()
     {
-        var_dump(scandir(__DIR__ . './vendor'));
+        var_dump(scandir(__DIR__ . 'vendor/'));
         //Consumer key,
         $oauth = new TwitterOAuth("rC3gP2pji5zoKoGf4FlUYdvaa", "TYIpFvcb9wR6SrpdxmMCPruiyJSPSDfJdLz6cAlNgqoyMcMq2j");
         $accesstoken = $oauth->oauth2('oauth2/token', ['grant_type' => 'client_credentials']);
