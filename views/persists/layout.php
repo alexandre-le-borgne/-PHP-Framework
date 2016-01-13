0e9ec082ba389d46891ef59758420982f8f19902
darkicex3 <maxime.junique@gmail.com>
2016-01-13 15:36:01 +0100

    home design

--- views/persists/layout.php
+++ views/persists/layout.php
@@ -12,20 +12,7 @@
         <?php $this->render('persists/head'); ?>
     </head>
     <body>
-        <div id="fb-root"></div>
-        <script>
-            (function(d, s, id) {
-                var js, fjs = d.getElementsByTagName(s)[0];
-                if (d.getElementById(id)) return;
-                js = d.createElement(s); js.id = id;
-                js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.5&appId=1148334785195709";
-                fjs.parentNode.insertBefore(js, fjs);
-            }(document, 'script', 'facebook-jssdk'));
-        </script>
-
         <div id="fullpage">
-
-            <div class="fb-login-button" data-max-rows="1" data-size="large" data-show-faces="false" data-auto-logout-link="false"></div>
             <div class="section">
                 <?php
                 echo $this->output('_content');
