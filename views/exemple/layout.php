8d133ac711358144bf831d30b94ffd4e4c20152b
Alexandre <alex83690@live.fr>
2016-01-13 14:22:05 +0100

    Alex

--- views/exemple/layout.php
+++ views/exemple/layout.php
@@ -16,7 +16,7 @@
      * On peut vérifier si l'utilisateur à un certain rang avec la fonction 'isGranted' et avec les constantes de Session.
      * La variable '$_content' contient le code html de la vue remplissant cette template.
      */
-    $view->render('exemple/head', array('title' => $title));
+    $this->render('exemple/head', array('title' => $title));
     ?>
     <body>
         <h1><?= $this->output('title', 'Titre par défaut') ?></h1>
