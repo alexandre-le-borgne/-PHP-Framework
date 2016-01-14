e317466369e0ce510c0892e711a2a4a8354d2754
j14003626 <dzdaazdza>
2016-01-14 09:01:03 +0100

    ddzadza

--- controller/TestTwitterController.php
+++ controller/TestTwitterController.php
@@ -13,6 +13,7 @@ class TestTwitterController extends Controller
 {
     function TwitterAction()
     {
+        var_dump(scandir('./vendor'));
         //Tout cela pour récupérer les tweets d'un compte donné
         //Consumer key,
         $oauth = new TwitterOAuth("rC3gP2pji5zoKoGf4FlUYdvaa", "TYIpFvcb9wR6SrpdxmMCPruiyJSPSDfJdLz6cAlNgqoyMcMq2j");
