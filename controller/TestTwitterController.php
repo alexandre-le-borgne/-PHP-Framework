3354d72eac18a9429e1d81db241974249d381e17
jjtheo <theo.jeanjean42@gmail.com>
2016-01-18 22:21:23 +0100

    Twitter

--- controller/TestTwitterController.php
+++ controller/TestTwitterController.php
@@ -6,19 +6,16 @@
 require './vendor/twitteroauth/autoload.php';
 
 use Abraham\TwitterOAuth\TwitterOAuth;
-//use Abraham\TwitterOAuth\TwitterOAuth;
-
-//require '/../vendor/abraham/twitteroauth/autoload.php';
 
 class TestTwitterController extends Controller
 {
     function TwitterAction()
     {
         //Tout cela pour récupérer les tweets d'un compte donné
-        //Consumer key,
+        //Consumer key, consumerSecret, oauthToken, oauthTokenSecret
         $oauth = new TwitterOAuth("rC3gP2pji5zoKoGf4FlUYdvaa", "TYIpFvcb9wR6SrpdxmMCPruiyJSPSDfJdLz6cAlNgqoyMcMq2j");
         $accesstoken = $oauth->oauth2('oauth2/token', ['grant_type' => 'client_credentials']);
-//        var_dump($accesstoken);
+        var_dump($accesstoken);
 
         $twitter = new TwitterOAuth("rC3gP2pji5zoKoGf4FlUYdvaa", "TYIpFvcb9wR6SrpdxmMCPruiyJSPSDfJdLz6cAlNgqoyMcMq2j", null, $accesstoken->access_token);
 
@@ -34,14 +31,14 @@ class TestTwitterController extends Controller
         ?>
         <ul class="white_text">
             <?php foreach (array_slice($tweets, 0, 12) as $tweet): ?>
-                <li><?php $this->render('layouts/tweetTemplate',$tweet->text); ?></li>
+                <li><?php $this->render('layouts/tweetTemplate', $tweet->text); ?></li>
             <?php endforeach; ?>
         </ul>
-    <?php
+        <?php
     }
 }
 
-//s
+
 //http://www.grafikart.fr/tutoriels/php/twitter-api-tweets-100
 //"token_type": "bearer",
 //"access_token": "AAAAAAAAAAAAAAAAAAAAAGszjwAAAAAAQ9wM2BLgs2H5JAbsI6Iv9gE6xDU%3DiXa5ZguYCBBgkWld7lIhMWzfPUHbpqDuwQgktXQ8qVoR8GjnOj"
