0214a41052c791ddc6f5e17bdfe8cada9dd5d5a6
jikar <julienjandot@orange.fr>
2016-01-18 22:50:19 +0100

    facebook

---
+++ views/layouts/facebook.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: julien
+ * Date: 18/01/2016
+ * Time: 21:54
+ */
+
+session_start();
+
+$appId = '1695359537375763';
+$appSecret = '038f3ed86d0e2e74ab627786aea08c25';
+
+$fb = new Facebook\Facebook([
+    'app_id' => $appId,
+    'app_secret' => $appSecret,
+    'default_graph_version' => 'v2.2',
+]);
+
+$helper = $fb->getRedirectLoginHelper();
+
+$permissions = ['email']; // Optional permissions
+$loginUrl = $helper->getLoginUrl('http://alex83690.alwaysdata.net/aaron/fb-callback.php', $permissions);
+
+echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
