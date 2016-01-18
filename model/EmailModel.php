ef990fb4659b11cec18511f7b7141f11ae398241
Alexandre <alex83690@live.fr>
2016-01-19 00:59:01 +0100

    Alex

--- model/EmailModel.php
+++ model/EmailModel.php
@@ -48,7 +48,7 @@ class EmailModel
         $message = array();
 
         $header = imap_header($mbox, $messageid);
-        $structure = imap_fetchstructure($mbox, $messageid);
+        $structure = imap_fetchstructure($mbox, $messageid, FT_UID);
 
         $message['subject'] = $header->subject;
         $message['fromaddress'] =   $header->fromaddress;
