5eb4d8865d4c81c019fc79aaa907c8dd650f7e84
Alexandre <alex83690@live.fr>
2016-01-18 23:52:14 +0100

    Alex

--- views/layouts/articles.php
+++ views/layouts/articles.php
@@ -8,9 +8,9 @@
  */
 ?>
 <?php foreach ($articles as $article) : var_dump($article);?>
-    <h1><?= $article . getTitle() ?></h1>
-    <h2><?= $article . getDate() ?></h2>
-    <p><?= $article . getContent() ?></p>
+    <h1><?= $article.getTitle() ?></h1>
+    <h2><?= $article.getDate() ?></h2>
+    <p><?= $article.getContent() ?></p>
     <br><br><hr><br><br>
 <?php endforeach; ?>
 
