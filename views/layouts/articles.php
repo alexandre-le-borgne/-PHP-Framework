e85fa6dd4117db469e8f93cbda7ef1523193367b
Alexandre <alex83690@live.fr>
2016-01-18 23:51:26 +0100

    Alex

--- views/layouts/articles.php
+++ views/layouts/articles.php
@@ -7,7 +7,7 @@
  * Time: 23:38
  */
 ?>
-<?php foreach ($articles as $article) : ?>
+<?php foreach ($articles as $article) : var_dump($article);?>
     <h1><?= $article . getTitle() ?></h1>
     <h2><?= $article . getDate() ?></h2>
     <p><?= $article . getContent() ?></p>
