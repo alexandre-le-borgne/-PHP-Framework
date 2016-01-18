b5ac2b0a49c2e147f050d27e0f356fd7789fae34
Alexandre <alex83690@live.fr>
2016-01-19 00:50:33 +0100

    Alex

--- model/EmailModel.php
+++ model/EmailModel.php
@@ -37,11 +37,67 @@ class EmailModel
         }
     }
 
-    function decode_qprint($str)
+    function decode_body($str)
     {
-        $str = quoted_printable_decode($str);
-        $str = iconv('UTF-8', 'UTF-8', $str);
-        return $str;
+        return trim( utf8_encode( quoted_printable_decode( $str)));
+    }
+
+    function flattenParts($messageParts, $flattenedParts = array(), $prefix = '', $index = 1, $fullPrefix = true) {
+
+        foreach($messageParts as $part) {
+            $flattenedParts[$prefix.$index] = $part;
+            if(isset($part->parts)) {
+                if($part->type == 2) {
+                    $flattenedParts = flattenParts($part->parts, $flattenedParts, $prefix.$index.'.', 0, false);
+                }
+                elseif($fullPrefix) {
+                    $flattenedParts = flattenParts($part->parts, $flattenedParts, $prefix.$index.'.');
+                }
+                else {
+                    $flattenedParts = flattenParts($part->parts, $flattenedParts, $prefix);
+                }
+                unset($flattenedParts[$prefix.$index]->parts);
+            }
+            $index++;
+        }
+
+        return $flattenedParts;
+
+    }
+
+    function getPart($body, $encoding) {
+        switch($encoding) {
+            case 0: return $body; // 7BIT
+            case 1: return $body; // 8BIT
+            case 2: return $body; // BINARY
+            case 3: return base64_decode($body); // BASE64
+            case 4: return quoted_printable_decode($body); // QUOTED_PRINTABLE
+            case 5: return $body; // OTHER
+        }
+    }
+
+    function getFilenameFromPart($part) {
+
+        $filename = '';
+
+        if($part->ifdparameters) {
+            foreach($part->dparameters as $object) {
+                if(strtolower($object->attribute) == 'filename') {
+                    $filename = $object->value;
+                }
+            }
+        }
+
+        if(!$filename && $part->ifparameters) {
+            foreach($part->parameters as $object) {
+                if(strtolower($object->attribute) == 'name') {
+                    $filename = $object->value;
+                }
+            }
+        }
+
+        return $filename;
+
     }
 
     public function getList()
@@ -53,12 +109,12 @@ class EmailModel
             "<br />\n" .
             "<br />\n";
         foreach ($mails as $mail) {
-            $headerText = imap_fetchHeader($this->conn, $mail->uid, FT_UID);
+            $headerText = imap_fetchHeader($this->conn, $mail->uid, FT_UID & FT_PEEK);
             $header = imap_rfc822_parse_headers($headerText);
-            $corps = imap_fetchbody($this->conn, $mail->uid, 1, FT_UID);
+            $corps = imap_fetchbody($this->conn, $mail->uid, 1, FT_UID & FT_PEEK);
             $article = new ArticleEntity();
             $article->setTitle($this->decode_qprint($mail->subject) . ' - ' . imap_utf8($header->from[0]->personal . ' [' . $header->from[0]->mailbox . '@' . $header->from[0]->host . ']'));
-            $article->setContent($this->decode_qprint($corps));
+            $article->setContent($this->decode_body($corps));
             $article->setDate(imap_utf8($mail->date));
             $articles[] = $article;
         }
