031d0053881ad230d89aa603e01931e9d45f5639
Alexandre <alex83690@live.fr>
2016-01-19 14:15:39 +0100

    Alex

--- views/layouts/validateInscription.php
+++ views/layouts/validateInscription.php
@@ -3,6 +3,6 @@
  * Page de confirmation d'inscription, est affichee une fois que
  * le compte a ete cree.
  */
-$this->extend('layouts/layout');
+$view->extend('layouts/layout');
 ?>
 Un email vous a été envoyé, rendez vous sur votre boite mail pour valider l'inscription
