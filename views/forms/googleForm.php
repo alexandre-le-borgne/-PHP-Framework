cd690a9bdebecb5731cf28953cdda9c2fb4fc6a1
Alexandre <alex83690@live.fr>
2016-01-19 23:44:32 +0100

    Alex

--- views/forms/googleForm.php
+++ views/forms/googleForm.php
@@ -5,7 +5,11 @@
  * Date: 19/01/2016
  * Time: 23:29
  */
-?>
-<a href="<?php echo $authUrl; ?>">
-    <img id="google_signin" src="http://ucollect.biz/wp-content/uploads/2015/03/Red-signin_Long_base_44dp.png" style="width: 100px;" >
-</a>
+if(isset($authUrl)) {
+    ?>
+    <a href="<?php echo $authUrl; ?>">
+        <img id="google_signin" src="http://ucollect.biz/wp-content/uploads/2015/03/Red-signin_Long_base_44dp.png"
+             style="width: 100px;">
+    </a>
+    <?php
+}
