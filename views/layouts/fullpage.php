d4accab7c98c77370621df1d8f1096f477839281
darkicex3 <maxime.junique@gmail.com>
2016-01-19 14:59:05 +0100

    google+ login

--- views/layouts/fullpage.php
+++ views/layouts/fullpage.php
@@ -23,25 +23,25 @@ $view->extend('layouts/layout');
         </script>
 
         <div id="my-signin2"></div>
-<!--        <script>-->
-<!--            function onSuccess(googleUser) {-->
-<!--                console.log('Logged in as: ' + googleUser.getBasicProfile().getName());-->
-<!--            }-->
-<!--            function onFailure(error) {-->
-<!--                console.log(error);-->
-<!--            }-->
-<!--            function renderButton() {-->
-<!--                gapi.signin2.render('my-signin2', {-->
-<!--                    'scope': 'https://www.googleapis.com/auth/plus.login',-->
-<!--                    'width': 200,-->
-<!--                    'height': 50,-->
-<!--                    'longtitle': true,-->
-<!--                    'theme': 'dark',-->
-<!--                    'onsuccess': onSuccess,-->
-<!--                    'onfailure': onFailure-->
-<!--                });-->
-<!--            }-->
-<!--        </script>-->
+        <script>
+            function onSuccess(googleUser) {
+                console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+            }
+            function onFailure(error) {
+                console.log(error);
+            }
+            function renderButton() {
+                gapi.signin2.render('my-signin2', {
+                    'scope': 'https://www.googleapis.com/auth/plus.login',
+                    'width': 200,
+                    'height': 50,
+                    'longtitle': true,
+                    'theme': 'dark',
+                    'onsuccess': onSuccess,
+                    'onfailure': onFailure
+                });
+            }
+        </script>
 
         <script src="https://apis.google.com/js/platform.js?onload=renderButton" async defer></script>
 
