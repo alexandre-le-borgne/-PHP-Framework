626115d931dc52c1dc56ded917807367a9703b6c
darkicex3 <maxime.junique@gmail.com>
2016-01-19 17:04:59 +0100

    google+ login

--- views/layouts/home.php
+++ views/layouts/home.php
@@ -5,6 +5,7 @@
  * Date: 19/01/2016
  * Time: 14:07
  */
-$view->extend('layouts/layout');
+$view->extend('layouts/layoutConnected');
 echo $this->output('home');
+
 ?>
---
+++ views/layouts/layoutConnected.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maxbook
+ * Date: 19/01/16
+ * Time: 17:04
+ */
+
+echo "Couilles !!!";
