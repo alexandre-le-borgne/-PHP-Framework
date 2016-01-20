844910b8a660cce93c918a8d606e25daeaac9eed
Alexandre <alex83690@live.fr>
2016-01-20 10:00:11 +0100

    Alex

--- controller/UserController.php
+++ controller/UserController.php
@@ -211,7 +211,7 @@ class UserController extends Controller
             exit;
         } catch(Facebook\Exceptions\FacebookSDKException $e) {
             // When validation fails or other local issues
-            throw new Exception ('Facebook SDK returned an error: ' . $e->getMessage());
+            throw new Exception ('Facebook SDK 2 returned an error: ' . $e->getMessage());
             exit;
         }
 
