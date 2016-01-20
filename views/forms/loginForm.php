103f14948f4f2b30cb42068c1eb332a670056564
Alexandre <alex83690@live.fr>
2016-01-20 10:42:22 +0100

    Alex

--- views/forms/loginForm.php
+++ views/forms/loginForm.php
@@ -28,9 +28,6 @@
     <div class="socialConnect">
         <!--            <button type="submit"><img src="web/img/fb_icon_325x325.png" class="img-rounded"></button>-->
         <!--            <button type="submit"><img src="web/img/share-googleplus.png" class="img-rounded"></button>-->
-        <div class="fb-login-button" data-max-rows="1" data-size="medium" data-show-faces="true"
-             data-auto-logout-link="true"></div>
-
         <?php
         $this->renderControllerAction('google');
         $this->renderControllerAction('facebook');
