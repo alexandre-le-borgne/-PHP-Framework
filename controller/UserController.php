ff3bbba077a2859a798524ec489bfecf40f117c9
Alexandre <alex83690@live.fr>
2016-01-20 09:59:34 +0100

    Alex

--- controller/UserController.php
+++ controller/UserController.php
@@ -242,7 +242,7 @@ class UserController extends Controller
         var_dump($tokenMetadata);
 
 // Validation (these will throw FacebookSDKException's when they fail)
-        $tokenMetadata->validateAppId($config['app_id']);
+        $tokenMetadata->validateAppId($appId);
 // If you know the user ID this access token belongs to, you can validate it here
 //$tokenMetadata->validateUserId('123');
         $tokenMetadata->validateExpiration();
