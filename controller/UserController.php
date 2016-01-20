f0e9efa5c5cfdd7fb49a5c7238a01a2f97b5ffe3
j13000355 <h>
2016-01-20 08:51:01 +0100

    fb

--- controller/UserController.php
+++ controller/UserController.php
@@ -10,8 +10,8 @@ $path = get_include_path();
 set_include_path( $path . PATH_SEPARATOR . 'vendor/google/apiclient/src' );
 require_once('Google/Client.php');
 require_once('Google/Auth/OAuth2.php');
-set_include_path( $path . PATH_SEPARATOR . 'vendor/facebook/php-sdk-v4/src' );
-var_dump(scandir($path . PATH_SEPARATOR . 'vendor/facebook/php-sdk-v4/src'));
+set_include_path( $path . '/vendor/facebook/php-sdk-v4/src' );
+var_dump(scandir($path .  '/vendor/facebook/php-sdk-v4/src'));
 
 require_once('Facebook/autoload.php');
 set_include_path($path);
