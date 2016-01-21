3b48c8d443648c76fb1244f15715c188d2b07f3e
j14003626 <daz>
2016-01-21 16:58:57 +0100

    Test tuitter

--- entity/TwitterEntity.php
+++ entity/TwitterEntity.php
@@ -6,7 +6,6 @@
  * Date: 14/01/16
  * Time: 09:49
  */
-
 class TwitterEntity
 {
     private $id;
@@ -14,24 +13,73 @@ class TwitterEntity
     private $lastUpdate;
     private $firstUpdate;
 
-    function __construct($id)
+
+    //Getters
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getChannel()
+    {
+        return $this->channel;
+    }
+
+    public function getLastUpdate()
+    {
+        return $this->lastUpdate;
+    }
+
+    public function getFirstUpdate()
+    {
+        return $this->firstUpdate;
+    }
+
+
+    //Setters
+    public function setId($id)
     {
         $this->id = $id;
-        if (intval($id))
-        {
-            $db = new Database();
-            $req = "Select * From stream_twitter Where Id = ?";
-            $result = $db->excecute($req, array($id))->fetch();
+    }
 
-            if ($result)
-            {
-                $this->id = $result['id'];
-                $this->channel = $result['channel'];
-                $this->lastUpdate = $result['lastUpdate'];
-                $this->firstUpdate = $result['firstUpdate'];
-                return;
-            }
-        }
-        throw new TraceableException("Flux twitter manquant");
+    public function setChannel($channel)
+    {
+        $this->channel = $channel;
+    }
+
+    public function setLastUpdate($lastUpdate)
+    {
+        $this->lastUpdate = $lastUpdate;
+    }
+
+    public function setFirstUpdate($firstUpdate)
+    {
+        $this->firstUpdate = $firstUpdate;
     }
 }
+
+
+/**
+ *
+ *
+ * function __construct($id)
+ * {
+ * $this->id = $id;
+ * if (intval($id))
+ * {
+ * $db = new Database();
+ * $req = "Select * From stream_twitter Where Id = ?";
+ * $result = $db->excecute($req, array($id))->fetch();
+ *
+ * if ($result)
+ * {
+ * $this->id = $result['id'];
+ * $this->channel = $result['channel'];
+ * $this->lastUpdate = $result['lastUpdate'];
+ * $this->firstUpdate = $result['firstUpdate'];
+ * return;
+ * }
+ * }
+ * throw new TraceableException("Flux twitter manquant");
+ *
+ */
