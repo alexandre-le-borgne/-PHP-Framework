1186926741dc3930ca917ec53e7adfa767f8f145
Alexandre <alex83690@live.fr>
2016-01-21 23:19:21 +0100

    Alex

--- controller/UserController.php
+++ controller/UserController.php
@@ -166,73 +166,41 @@ class UserController extends Controller
         }
     }
 
-    public function PreRegisterAction(Request $request)
+    public function RegisterAction(Request $request)
     {
-        $this->loadModel('UserModel');
-
         $email = $request->post('email');
         $password = $request->post('password');
         $confirmPwd = $request->post('confirmPwd');
-
-        if ((!$email && $password && $confirmPwd)) {
-            $this->render('index');
-            return;
-        }
-
-        $errors = array();
-        if ($this->usermodel->availableEmail($email) == UserModel::ALREADY_USED_EMAIL) {
-            $errors['email'] = 'Email déjà utilisé';
-        }
-        if ($this->usermodel->availableEmail($email) == UserModel::BAD_EMAIL_REGEX) {
-            $errors['email'] = 'Format d\'email incorrect';
-        }
-        if ($password != $confirmPwd) {
-            $errors['password'] = 'Mot de passe différent';
-        }
-        if (!($this->usermodel->correctPwd($password))) {
-            $errors['password'] = 'La taille du mdp doit être entre 6 et 20';
-        }
-        if (!(empty($errors))) {
-            $data = array('errors' => $errors);
-            $this->render('persists/home', $data);
-            return;
-        }
-
-        $request->getSession()->set('email', $email);
-        $request->getSession()->set('password', $password);
-
-        $this->render('forms/registerForm');
-    }
-
-    public function RegisterAction(Request $request)
-    {
         $username = $request->post('username');
-        //On recupere les champs deja entres
-        $email = $request->getSession()->get('email');
-        $password = $request->getSession()->get('password');
-
-        if ($username) {
+        if ($username && $email && $password && $confirmPwd) {
             $this->loadModel('UserModel');
 
             $errors = array();
-            $isError = false;
-            $data = array
-            (
-                'username' => $username,
-            );
-
+            if ($this->usermodel->availableEmail($email) == UserModel::ALREADY_USED_EMAIL) {
+                $errors['email'] = 'Email déjà utilisé';
+            }
+            if ($this->usermodel->availableEmail($email) == UserModel::BAD_EMAIL_REGEX) {
+                $errors['email'] = 'Format d\'email incorrect';
+            }
+            if ($password != $confirmPwd) {
+                $errors['password'] = 'Mot de passe différent';
+            }
+            if (!($this->usermodel->correctPwd($password))) {
+                $errors['password'] = 'La taille du mdp doit être entre 6 et 20';
+            }
             if (!($this->usermodel->availableUser($username))) {
                 $errors['username'] = 'Pseudonyme déjà utilisé';
-                $isError = true;
             }
-            if ($isError) {
-                $data['errors'] = $errors;
+            if (!(empty($errors))) {
+                $data = array('errors' => $errors);
                 $this->render('forms/registerForm', $data);
                 return;
             }
-
             $this->usermodel->addUser($username, $email, $password);
-            $this->render('layouts/validateInscription');
+            $this->redirectToRoute('index');
+        }
+        else {
+            $this->render('forms/registerForm');
         }
     }
 
