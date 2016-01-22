50086e00232905aeea13627f6f9132b91cfc1b31
j13000355 <g>
2016-01-22 11:07:56 +0100

    cron

--- model/RssModel.php
+++ model/RssModel.php
@@ -81,6 +81,8 @@ class RssModel extends Model implements StreamModel
             $req = "SELECT * FROM article WHERE stream_id = ? AND articleDate BETWEEN ? and ?";
             $result = $db->execute($req, array($stream_id, $streamFirst, $minDate));
 
+            var_dump($result->fetch());
+
 
             while($verif = $result->fetch()) {
                 echo "t'es pd?";
