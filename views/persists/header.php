33f5e81ae07b989967533029654a90cd07b4ab87
darkicex3 <maxime.junique@gmail.com>
2016-01-22 10:56:24 +0100

    ugyj

--- views/persists/header.php
+++ views/persists/header.php
@@ -22,7 +22,7 @@
             <?php
         }
         ?>
-        <a href="<? View::getUrlFromRoute('index') ?>"><img src="web/img/aaron-logo.png"></a>
+        <a id="logo_home" href="<? View::getUrlFromRoute('index') ?>"><img id="logo_aaron_home" src="web/img/aaron-logo.png"></a>
         <form method="post" action="search">
             <input class="search_bar" type="textarea" name="search" placeholder="Votre recherche...">
             <input type="submit" style="display: none">
