2c7c74f65aa42b7675462042d2b1c8dbb1b35fca
darkicex3 <maxime.junique@gmail.com>
2016-01-23 17:24:12 +0100

    layout_connected ultra fucking fatnessing

--- views/layouts/ajouterFlux.php
+++ views/layouts/ajouterFlux.php
@@ -38,7 +38,7 @@ $this->render('persists/header');
 
 <script>
     $(function() {
-        $('a').webuiPopover({width:300,height:200,placement:'bottom',trigger:'active'});
+        $('a').webuiPopover({width:300,height:200,placement:'bottom',trigger:'hover'});
 
         $('.flux_twitter').show();
         $('.flux_rss').hide();
