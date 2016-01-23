b71e221bc4620a521814b9fcdff27c8fe855b1ec
darkicex3 <maxime.junique@gmail.com>
2016-01-24 00:09:51 +0100

    layout_connected ultra fucking fatnessing

--- views/persists/feed.php
+++ views/persists/feed.php
@@ -40,7 +40,7 @@
                                 echo '<img src="' . View::getAsset('img/email.png') . '" ' . 'width="60" ' . '"';
                                 break;
                             case ArticleModel::RSS:
-                                echo '<img src="' . View::getAsset('img/rss.png') . '" ' . 'width="42" ' . '"';
+                                echo '<img src="' . View::getAsset('img/rss.png') . '" ' . 'width="25" ' . '"';
                                 break;
                             case ArticleModel::TWITTER:
                                 echo '<img src="' . View::getAsset('img/twitter.png') . '" ' . 'width="50" ' . '"';
