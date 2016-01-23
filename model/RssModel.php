da64e3cdf04c13febd61f2e12e5d887eee1b676b
jikar <julienjandot@orange.fr>
2016-01-23 14:14:00 +0100

    rss

--- model/RssModel.php
+++ model/RssModel.php
@@ -81,9 +81,9 @@ class RssModel extends Model implements StreamModel
             $req = "SELECT * FROM article WHERE stream_id = ? AND articleDate BETWEEN ? and ?";
             $result = $db->execute($req, array($stream_id, $streamFirst, $minDate));
             $result->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, 'ArticleEntity');
-            $verif = $result->fetch();
 
-            if(!$verif) {
+
+            if(!$verif = $result->fetch()) {
 
                 //$req = "SELECT content FROM article WHERE stream_id = ?";
                 foreach ($x->channel->item as $item) {
