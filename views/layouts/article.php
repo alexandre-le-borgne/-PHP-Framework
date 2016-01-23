2ec524c47b0f0d84ab718dfa75fb572b2120b6f7
Alexandre <alex83690@live.fr>
2016-01-23 22:33:05 +0100

    Alex

--- views/layouts/article.php
+++ views/layouts/article.php
@@ -2,9 +2,8 @@
 $view->extend('layouts/layout');
 $this->render('persists/header');
 ?>
-    <div id="layout_connected">
-        <?php
-        $this->render('persists/feed', array('articles' => $article));
-        ?>
-    </div>
-<?php
+<div id="layout_connected">
+    <?php
+    $this->render('persists/feed', array('articles' => $article));
+    ?>
+</div>
