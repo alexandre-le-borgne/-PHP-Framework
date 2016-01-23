8411e01f3e857955b95673e8eac49997c002df59
darkicex3 <maxime.junique@gmail.com>
2016-01-23 19:00:56 +0100

    layout_connected ultra fucking fatnessing

--- views/persists/feed.php
+++ views/persists/feed.php
@@ -38,6 +38,7 @@
                     preg_match('#<a[^>]+>#', htmlentities($article->getContent()), $result);
                     print_r($result);
                 }
+
                 else {
                     echo $article->getContent();
                 }
