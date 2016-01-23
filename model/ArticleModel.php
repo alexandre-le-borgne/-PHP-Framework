04217d704d678452bbf6a10119a340024189524f
Alexandre <alex83690@live.fr>
2016-01-23 18:10:21 +0100

    Alex

--- model/ArticleModel.php
+++ model/ArticleModel.php
@@ -42,6 +42,7 @@ class ArticleModel extends Model
             $req = "SELECT article.* FROM article JOIN stream_category ON article.stream_id = stream_category.stream AND article.streamType = stream_category.streamType WHERE stream_category.category IN (SELECT id FROM categories WHERE account = ?) ORDER BY articleDate DESC LIMIT ?, ?";
             var_dump($req);
             $data = $db->execute($req, array($user, $start, $len));
+            var_dump($data->queryString);
             $data->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, 'ArticleEntity');
             return $data->fetchAll();
         }
