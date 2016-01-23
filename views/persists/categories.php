27739edd0df7cd2ffabd18c693d4049d7b538f92
Alexandre <alex83690@live.fr>
2016-01-23 16:50:58 +0100

    Alex

--- views/persists/categories.php
+++ views/persists/categories.php
@@ -12,7 +12,13 @@
     <a class="item_cat" href="<?= View::getUrlFromRoute('categoryadd') ?>"><img id="add_cat" src="web/img/add_cat.png"></a>
     <?php
     /** @var CategoryEntity $category */
-    foreach($categories as $category): ?>
-        <a class="item_cat" href="<?= View::getUrlFromRoute('streamadd') ?>">Ajouter un flux</a>
-    <?php endforeach; ?>
+    if(!empty($categories)) {
+        foreach($categories as $category)
+        {
+            ?>
+            <a class="item_cat" href="<?= View::getUrlFromRoute('streamadd') ?>">Ajouter un flux</a>
+            <?php
+        }
+    }
+    ?>
 </div>
