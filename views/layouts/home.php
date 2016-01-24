c250b333ba96cbf65cc0eaef67cc25e4fabb802b
Alexandre <alex83690@live.fr>
2016-01-24 17:54:38 +0100

    Alex

--- views/layouts/home.php
+++ views/layouts/home.php
@@ -30,6 +30,7 @@ echo $this->output('_content');
         <?php
     if (isset($categories))
         $this->render('persists/categories', array('categories' => $categories));
+    echo '<br><hr>';
     if (isset($streams))
         $this->render('persists/streams', array('streams' => $streams));
     ?>
