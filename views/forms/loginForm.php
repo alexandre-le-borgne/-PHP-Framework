d00e8ecd0853fb50f88a3d42da30dbc9b5360404
darkicex3 <maxime.junique@gmail.com>
2016-01-24 16:02:54 +0100

    FULL PAGE - RESPONSIVE

--- views/forms/loginForm.php
+++ views/forms/loginForm.php
@@ -61,9 +61,9 @@ $view->extend('layouts/fullpage');
             var largeur_fenetre = $(window).width();
             var hauteur_fenetre = $(window).height();
 
-            var haut = (hauteur_fenetre - element.outerHeight) / 2 + $(window).scrollTop();
-            var gauche = (largeur_fenetre - element.outerWidth) / 2 + $(window).scrollLeft();
-            element.css({position: 'relative', top: haut, left: gauche});
+            var haut = (hauteur_fenetre - $(element).height()) / 2 + $(window).scrollTop();
+            var gauche = (largeur_fenetre - $(element).width()) / 2 + $(window).scrollLeft();
+            $(element).css({position: 'relative', top: haut, left: gauche});
         }
 
         centrerElementAbsolu('.loginDiv');
