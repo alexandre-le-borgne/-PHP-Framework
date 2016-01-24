9f6c113ec9735e998bc4a15a6e70dafd61c95e49
Alexandre <alex83690@live.fr>
2016-01-24 02:02:14 +0100

    Alex

--- controller/AjaxController.php
+++ controller/AjaxController.php
@@ -8,5 +8,39 @@
  */
 class AjaxController extends Controller
 {
+    public function IndexAction(Request $request) {
+        if(!$request->getSession()->isGranted(Session::USER_IS_CONNECTED))
+            return;
+        switch($request->post('action')) {
+            case 'like':
+                $this->LikeAction();
+                break;
+            case 'nolike':
+                $this->NoLikeAction();
+                break;
+            default:
+        }
+    }
 
+    private function LikeAction(Request $request) {
+        $post = ltrim(strstr($request->post('id'), '_'), '_');
+        $this->loadModel('CategoryModel');
+        $this->loadModel('ArticleModel');
+        /** @var ArticleEntity $articleEntity */
+        $articleEntity = $this->articlemodel->getById($post);
+        if($articleEntity) {
+            $articlesFavorisEntity = new ArticlesFavorisEntity();
+            $articlesFavorisEntity->setAccount($request->getSession()->get('id'));
+            $articlesFavorisEntity->setArticle($articleEntity->getId());
+            $articlesFavorisEntity->persist();
+        }
+    }
+
+    private function NoLikeAction(Request $request) {
+        $post = ltrim(strstr($request->post('id'), '_'), '_');
+        $this->loadModel('CategoryModel');
+        $this->loadModel('ArticleModel');
+        /** @var ArticleEntity $articleEntity */
+        $this->articlemodel->removeFromFavoris($request->getSession()->get('id'), $post);
+    }
 }
