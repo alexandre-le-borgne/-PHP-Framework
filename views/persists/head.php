58ece15cdbc1ba319f8dc18c686781b74ebb0951
darkicex3 <maxime.junique@gmail.com>
2016-01-24 18:03:06 +0100

    FULL PAGE - RESPONSIVE

--- views/persists/head.php
+++ views/persists/head.php
@@ -11,18 +11,18 @@
 <link rel="stylesheet" href="http://code.jquery.com/ui/1.9.0/themes/smoothness/jquery-ui.css" />
 <link rel="stylesheet" href="<?= View::getAsset('inc/jquery.webui-popover.css') ?>" />
 <link rel="stylesheet" href="<?= View::getAsset('inc/jquery.mCustomScrollbar.min.css') ?>" />
-<link rel="stylesheet" media="screen" href="<?= View::getAsset('css/desktop/form.css') ?>">
-<link rel="stylesheet" media="screen" href="<?= View::getAsset('css/desktop/body.css') ?>">
-<link rel="stylesheet" media="screen" href="<?= View::getAsset('css/desktop/fullpage.css') ?>">
-<link rel="stylesheet" media="screen" href="<?= View::getAsset('css/desktop/footer.css') ?>">
-<link rel="stylesheet" media="screen" href="<?= View::getAsset('css/desktop/nav.css') ?>">
-<link rel="stylesheet" media="screen" href="<?= View::getAsset('css/desktop/header.css') ?>">
-<link rel="stylesheet" media="screen" href="<?= View::getAsset('css/desktop/post.css') ?>">
-<link rel="stylesheet" media="screen" href="<?= View::getAsset('css/desktop/home.css') ?>">
+<!--<link rel="stylesheet" media="screen" href="--><?//= View::getAsset('css/desktop/form.css') ?><!--">-->
+<!--<link rel="stylesheet" media="screen" href="--><?//= View::getAsset('css/desktop/body.css') ?><!--">-->
+<!--<link rel="stylesheet" media="screen" href="--><?//= View::getAsset('css/desktop/fullpage.css') ?><!--">-->
+<!--<link rel="stylesheet" media="screen" href="--><?//= View::getAsset('css/desktop/footer.css') ?><!--">-->
+<!--<link rel="stylesheet" media="screen" href="--><?//= View::getAsset('css/desktop/nav.css') ?><!--">-->
+<!--<link rel="stylesheet" media="screen" href="--><?//= View::getAsset('css/desktop/header.css') ?><!--">-->
+<!--<link rel="stylesheet" media="screen" href="--><?//= View::getAsset('css/desktop/post.css') ?><!--">-->
+<!--<link rel="stylesheet" media="screen" href="--><?//= View::getAsset('css/desktop/home.css') ?><!--">-->
 
-<link rel="stylesheet" media="handheld" href="<?= View::getAsset('css/responsive/form_responsive.css') ?>">
-<link rel="stylesheet" media="handheld" href="<?= View::getAsset('css/responsive/body_responsive.css') ?>">
-<link rel="stylesheet" media="handheld" href="<?= View::getAsset('css/responsive/fullpage_responsive.css') ?>">
+<link rel="stylesheet" media="only screen and (max-device-width:480px)" href="<?= View::getAsset('css/responsive/form_responsive.css') ?>">
+<link rel="stylesheet" media="only screen and (max-device-width:480px)" href="<?= View::getAsset('css/responsive/body_responsive.css') ?>">
+<link rel="stylesheet" media="only screen and (max-device-width:480px)" href="<?= View::getAsset('css/responsive/fullpage_responsive.css') ?>">
 
 
 
