15caccf09a5ba5843aadf6d33737a3e85bb6b312
jjtheo <theo.jeanjean42@gmail.com>
2016-01-24 03:09:52 +0100

    Modif AdminController

--- views/layouts/profile.php
+++ views/layouts/profile.php
@@ -5,6 +5,6 @@
  * Date: 24/01/16
  * Time: 02:47
  */
-$view->extend('layouts/layout');
-$this->render('persists/header');
+//$view->extend('layouts/layout');
+//$this->render('persists/header');
 var_dump($data);
