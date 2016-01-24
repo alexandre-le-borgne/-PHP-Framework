ded2473333d5cf49a864194ff06939c65ce7a609
jjtheo <theo.jeanjean42@gmail.com>
2016-01-24 13:26:55 +0100

    Pour toi julien <3

--- views/layouts/admin/adminDashboard.php
+++ views/layouts/admin/adminDashboard.php
@@ -2,6 +2,9 @@
 /**
  * Le pannel admin, qui mermet de supprimer, et voir tous les comptes utilisateur
  */
+
+$view->extend('layouts/layout');
+$this->render('persists/header');
 ?>
 
 <a class="manageUsers" href="<?= View::getUrlFromRoute('adminusers') ?>">Gérer les utilisateurs</a>
