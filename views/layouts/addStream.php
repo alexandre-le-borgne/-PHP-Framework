05cb7fd163dc9b714abf48214607f7c8447433b5
darkicex3 <maxime.junique@gmail.com>
2016-01-24 03:02:56 +0100

    layout_connected ultra fucking fatnessing

--- views/layouts/addStream.php
+++ views/layouts/addStream.php
@@ -17,20 +17,20 @@ $this->render('persists/header');
     </div>
     <div id="add_flux_body">
         <form class="flux_twitter" action="" method="post">
-            <input type="date" name="firstUpdate" placeholder="À partir de" required>
+            <input class="first" type="date" name="firstUpdate" placeholder="À partir de" required>
             <input type="text" name="channel" placeholder="Nom du compte @" required>
-            <input type="text" name="category" placeholder="La catégorie pour le ranger" required>
+            <input class="last" type="text" name="category" placeholder="La catégorie pour le ranger" required>
         </form>
 
         <form class="flux_rss" action="" method="post">
-            <input type="text" name="url_flux" placeholder="URL du flux" required>
+            <input class="first last"type="text" name="url_flux" placeholder="URL du flux" required>
         </form>
 
         <form class="flux_imap" action="" method="post">
-            <input type="text" name="host_name" placeholder="Serveur IMAP" required>
+            <input class="first" type="text" name="host_name" placeholder="Serveur IMAP" required>
             <input type="text" name="username" placeholder="Nom d'utilisateur" required>
             <input type="password" name="password" placeholder="Mot de passe" required>
-            <input type="date" name="since_date" placeholder="À partir de" required>
+            <input class="last" type="date" name="since_date" placeholder="À partir de" required>
         </form>
     </div>
 </div>
