a064f1c70ea11075a1d301f89d8167413b9a4f09
Alexandre <alex83690@live.fr>
2016-01-24 03:40:30 +0100

    Alex

--- controller/ArticleController.php
+++ controller/ArticleController.php
@@ -41,7 +41,6 @@ class ArticleController extends Controller
                 $categories = $this->categorymodel->getByUserId($request->getSession()->get('id'));
                 $articles = $this->articlemodel->getArticlesByCategoryId($categoryEntity->getId(), 0, 50);
                 $favoris = $this->articlemodel->getIdOfFavoris($request->getSession()->get('id'));
-                var_dump($favoris);
                 $data = array('title' => $categoryEntity->getTitle(), 'categories' => $categories, 'articles' => $articles, 'favoris' => $favoris);
                 $this->render('layouts/home', $data);
             }
@@ -58,7 +57,8 @@ class ArticleController extends Controller
         $article = $this->articlemodel->getById($id);
         if($article)
         {
-            $this->render('layouts/article', array('article' => $article));
+            $favoris = $this->articlemodel->getIdOfFavoris($request->getSession()->get('id'));
+            $this->render('layouts/article', array('article' => $article, 'favoris' => $favoris));
         }
         else {
             $this->redirectToRoute('index');
